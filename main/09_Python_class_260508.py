e8883b7e10902ebcbe44f6c2468cca4df3821fb8
ks804 <daemanchanel@gmail.com>
2026-05-08 10:21:56 +0900

    Delete main/09_Python_class_260508.py

--- main/09_Python_class_260508.py
+++
@@ -1,9 +0,0 @@
-class Circle:
-    pi = 3.14
-    def __init__(self, radius):
-        self.radius = radius
-        def area(self):
-            return self.radius ** 2 * Circle.pi
-        
-c = Circle(2.5)
-print(c.area())
