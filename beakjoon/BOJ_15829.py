46aa330a2385062446d71af5a54b76225658a7ad
ks804 <daemanchanel@gmail.com>
2026-06-15 09:01:05 +0900

    Update BOJ_15829.py

--- beakjoon/BOJ_15829.py
+++ beakjoon/BOJ_15829.py
@@ -5,7 +5,7 @@ m = 1234567891
 hash = 0 
 
 for i in range(len):
-    cur = ord(str[i]) - 96  # 문자를 숫자로 변환
-    hash += cur * (r ** i)  # 다항식 해시 계산
+    cur = ord(str[i]) - 96 
+    hash += cur * (r ** i) 
 
-print(hash % m)  # 최종 해시 값 출력
+print(hash % m) 
